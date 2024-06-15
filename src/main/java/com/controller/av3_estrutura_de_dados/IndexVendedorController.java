e7272e85195306674b2529af7d4a75befc777ae8
brenno <brennorebeiro@gmail.com>
2024-06-15 12:45:58 -0300

    Add controller indexVendedor

--- src/main/java/com/controller/av3_estrutura_de_dados/IndexVendedorController.java
+++ src/main/java/com/controller/av3_estrutura_de_dados/IndexVendedorController.java
@@ -1,2 +1,20 @@
-package com.controller.av3_estrutura_de_dados;public class IndexVendedorController {
+package com.controller.av3_estrutura_de_dados;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+
+public class IndexVendedorController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
 }
