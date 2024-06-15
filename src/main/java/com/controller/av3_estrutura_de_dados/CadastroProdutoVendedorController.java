a20e29dd02ff3b89c7691782b3f87daf5e39693f
brenno <brennorebeiro@gmail.com>
2024-06-15 19:27:51 -0300

    Add controller

--- src/main/java/com/controller/av3_estrutura_de_dados/CadastroProdutoVendedorController.java
+++ src/main/java/com/controller/av3_estrutura_de_dados/CadastroProdutoVendedorController.java
@@ -1,32 +1,26 @@
 package com.controller.av3_estrutura_de_dados;
 
-import java.io.IOException;
 import java.net.URL;
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
-
-import javafx.scene.control.Button;
 
 import com.models.av3_estrutura_de_dados.Entities.Listas.ListaClientes;
 import com.models.av3_estrutura_de_dados.Entities.Pilhas.PilhaProdutos;
 import com.controller.av3_estrutura_de_dados.interfaces.Controller;
-import com.views.av3_estrutura_de_dados.util.CarregarPagina;
-import com.views.av3_estrutura_de_dados.CadastroProdutoVendedor;
 
-public class IndexVendedorController implements Initializable, Controller {
+public class CadastroProdutoVendedorController implements Initializable, Controller {
+
     @FXML
     private ListaClientes listaClientes=null;
     @FXML
     private PilhaProdutos pilhaProdutos=null;
     @FXML
     private Label labelNomeUsuario;
-    @FXML
-    private Button btnCadastroProduto;
 
     @Override
     public void setListaClientes(ListaClientes listaClientes){
@@ -39,21 +33,10 @@ public class IndexVendedorController implements Initializable, Controller {
         this.pilhaProdutos = pilhaProdutos;
     }
 
+    @Override
     public void getPilhaProdutos(){
         System.out.println("==========================");
         this.pilhaProdutos.mostrarProdutos();
-    }
-
-    @FXML
-    public void onBtnCadastroProdutoOnAction(ActionEvent event) throws IOException {
-        try{
-
-            CarregarPagina.trocarPagina(event, CadastroProdutoVendedor.class,
-                    "CadastroProdutoVendedor-view.fxml", this.listaClientes,
-                    this.pilhaProdutos);
-        }catch (RuntimeException e){
-            e.printStackTrace();
-        }
     }
 
     @Override
--- src/main/java/com/views/av3_estrutura_de_dados/CadastroProdutoVendedor.java
+++ src/main/java/com/views/av3_estrutura_de_dados/CadastroProdutoVendedor.java
@@ -5,17 +5,24 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class CadastroProdutoVendedor extends Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(CadastroProdutoVendedor.class.getResource(
-                "CadastroProdutoVendedor-view.fxml"));
-        Scene scene = new Scene(loader.load());
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException {
+        try{
+            FXMLLoader loader = new FXMLLoader(CadastroProdutoVendedor.class.getResource(
+                    "CadastroProdutoVendedor-view.fxml"));
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+            stage.show();
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+
     }
 }
