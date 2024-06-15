f8937ad61fcfd91c7c63e1057ee3f3985d604588
brenno <brennorebeiro@gmail.com>
2024-06-15 17:08:19 -0300

    Add metodos

--- src/main/java/com/models/av3_estrutura_de_dados/Entities/Pilhas/PilhaProdutos.java
+++ src/main/java/com/models/av3_estrutura_de_dados/Entities/Pilhas/PilhaProdutos.java
@@ -1,2 +1,17 @@
-package com.models.av3_estrutura_de_dados.Entities.Pilhas.NosPilhas;public class PilhaProdutos {
+package com.models.av3_estrutura_de_dados.Entities.Pilhas;
+
+import com.models.av3_estrutura_de_dados.Entities.Pilhas.NosPilhas.NoPilhaProduto;
+
+
+public class PilhaProdutos {
+    public NoPilhaProduto base;
+    public NoPilhaProduto topo;
+
+    public PilhaProdutos() {
+        this.base = this.topo = null;
+    }
+
+    public Boolean pilhaEstaVazia(){
+        return  this.base == null && this.topo == null;
+    }
 }
