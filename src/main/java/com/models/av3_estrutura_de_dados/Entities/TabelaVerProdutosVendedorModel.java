64f08bc5672847021561bb27728dfef2bd5f6feb
brenno <brennorebeiro@gmail.com>
2024-06-15 23:12:08 -0300

    Add model para tabela produtos

---
+++ src/main/java/com/models/av3_estrutura_de_dados/Entities/TabelaVerProdutosVendedorModel.java
@@ -0,0 +1,7 @@
+package com.models.av3_estrutura_de_dados.Entities;
+
+public class TabelaVerProdutosVendedorModel {
+    public String nome;
+    public double preco;
+    public int quantidade;
+}
