35a700389c00fc042a1315791d2df2bf3db33f45
brenno <brennorebeiro@gmail.com>
2024-06-17 11:28:52 -0300

    Add Produto A avaliar view

---
+++ src/main/java/com/views/av3_estrutura_de_dados/ProdutosAAvaliar.java
@@ -0,0 +1,25 @@
+package com.views.av3_estrutura_de_dados;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ProdutosAAvaliar extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader(IndexVendedor.class.getResource("IndexCliente-view.fxml"));
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+            stage.show();
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+    }
+}
