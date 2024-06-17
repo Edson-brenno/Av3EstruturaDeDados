6849cd4108ace314c06b4216128b9d548781e28e
brenno <brennorebeiro@gmail.com>
2024-06-17 09:53:39 -0300

    Add fila Avaliacao

--- src/main/java/com/models/av3_estrutura_de_dados/Entities/Filas/FilaAvaliacaoPedido.java
+++ src/main/java/com/models/av3_estrutura_de_dados/Entities/Filas/FilaAvaliacaoPedido.java
@@ -1,4 +1,27 @@
 package com.models.av3_estrutura_de_dados.Entities.Filas;
 
+import com.models.av3_estrutura_de_dados.Entities.Filas.NosFilas.NoFilaAvaliacaoPedido;
+
 public class FilaAvaliacaoPedido {
+    private NoFilaAvaliacaoPedido inicio;
+    private NoFilaAvaliacaoPedido fim;
+
+    public FilaAvaliacaoPedido() {
+        this.inicio = this.fim = null;
+    }
+
+    public NoFilaAvaliacaoPedido getInicio() {
+        return this.inicio;
+    }
+
+    public void setInicio(NoFilaAvaliacaoPedido inicio) {
+        this.inicio = inicio;
+    }
+
+    public NoFilaAvaliacaoPedido getFim() {
+        return this.fim;
+    }
+    public void setFim(NoFilaAvaliacaoPedido fim) {
+        this.fim = fim;
+    }
 }
