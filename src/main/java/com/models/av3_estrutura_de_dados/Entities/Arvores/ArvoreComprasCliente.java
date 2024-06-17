0937ba688bcf8dd266207f727bd166d943cd1603
brenno <brennorebeiro@gmail.com>
2024-06-16 21:41:55 -0300

    Add metodo adicionar

--- src/main/java/com/models/av3_estrutura_de_dados/Entities/Arvores/ArvoreComprasCliente.java
+++ src/main/java/com/models/av3_estrutura_de_dados/Entities/Arvores/ArvoreComprasCliente.java
@@ -3,5 +3,43 @@ package com.models.av3_estrutura_de_dados.Entities.Arvores;
 import com.models.av3_estrutura_de_dados.Entities.Arvores.NosAvores.NoAvoreCompraCliente;
 
 public class ArvoreComprasCliente {
+    private NoAvoreCompraCliente raiz;
 
+    public ArvoreComprasCliente() {
+        this.raiz = null;
+    }
+
+    public NoAvoreCompraCliente getRaiz() {
+        return this.raiz;
+    }
+
+    private boolean arvoreEstaVazia(){
+        return this.raiz == null;
+    }
+    public void adicionarCompraCliente(String nomeProduto, double precoProduto, int idVendedor, int idCliente) {
+        NoAvoreCompraCliente novoNo = new NoAvoreCompraCliente(nomeProduto, precoProduto, idVendedor, idCliente);
+        if (this.arvoreEstaVazia()) {
+            this.raiz = novoNo;
+        }else{
+            NoAvoreCompraCliente atual = this.raiz;
+
+            while(true){
+                if(novoNo.getPrecoProduto() < atual.getPrecoProduto()){
+                    if(atual.getEsquerda() != null){
+                        atual = atual.getEsquerda();
+                    }else{
+                        atual.setEsquerda(novoNo);
+                        break;
+                    }
+                }else{
+                    if(atual.getDireita() != null){
+                        atual = atual.getDireita();
+                    }else{
+                        atual.setDireita(novoNo);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
