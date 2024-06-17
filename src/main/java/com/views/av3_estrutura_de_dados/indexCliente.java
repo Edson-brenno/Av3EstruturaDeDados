2e7d4e4881b0a8d85f24544256e771e42534ed56
brenno <brennorebeiro@gmail.com>
2024-06-16 22:45:52 -0300

    Add view produtos

---
+++ src/main/java/com/views/av3_estrutura_de_dados/indexCliente.java
@@ -0,0 +1,27 @@
+package com.views.av3_estrutura_de_dados;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class indexCliente extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader loader = new FXMLLoader(IndexVendedor.class.getResource("IndexCliente-view.fxml"));
+            Scene scene = new Scene(loader.load());
+            stage.setScene(scene);
+            stage.show();
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+    }
+}
